6381f64363343e15370e5780c0ddc4ec2211ee8e
Nicolae Bacila <nicolae.bacila@infiniswiss.com>
2019-06-12 07:12:45 +0300

    asda

--- components/swipe-to-remove/swipe-actions.tsx
+++ components/swipe-to-remove/swipe-actions.tsx
@@ -5,147 +5,180 @@ import {Animated, GestureResponderEvent, PanResponder, PanResponderGestureState,
 import {SwipeActionsStyles} from './swipe-actions.styles';
 
 interface SwipeToRemoveProps {
-  elementWidth: number;
-  elementBackgroundColor?: string;
-  elementSwipingBackgroundColor?: string;
-  actionIcon?: string;
-  actionIconColor?: string;
-  actionIconSwipeLeft?: string;
-  actionIconSwipeLeftColor?: string;
-  actionIconSwipeRight?: string;
-  actionIconSwipeRightColor?: string;
-  enableSwipe?: boolean;
-  enableSwipeLeft?: boolean;
-  enableSwipeRight?: boolean;
-  onSwipeEnd?: () => void;
-  onSwipeLeftEnd?: () => void;
-  onSwipeRightEnd?: () => void;
+    elementWidth: number;
+    elementBackgroundColor?: string;
+    elementSwipingBackgroundColor?: string;
+    actionIcon?: string;
+    actionIconColor?: string;
+    actionIconSwipeLeft?: string;
+    actionIconSwipeLeftColor?: string;
+    actionIconSwipeRight?: string;
+    actionIconSwipeRightColor?: string;
+    animationDuration?: number;
+    enableSwipe?: boolean;
+    enableSwipeLeft?: boolean;
+    enableSwipeRight?: boolean;
+    onSwipeEnd?: () => void;
+    onSwipeLeftEnd?: () => void;
+    onSwipeRightEnd?: () => void;
 }
 
 interface SwipeToRemoveState {
-  position: Animated.ValueXY;
-  onSwipeBackgroundColor: string;
-  enableSwipe: boolean;
-  enableSwipeLeft: boolean;
-  enableSwipeRight: boolean;
+    position: Animated.ValueXY;
+    animated: Animated.Value;
+    onSwipeBackgroundColor: string;
+    animationDuration: number;
+    enableSwipe: boolean;
+    enableSwipeLeft: boolean;
+    enableSwipeRight: boolean;
 }
 
 export class SwipeActions extends PureComponent<SwipeToRemoveProps, SwipeToRemoveState> {
-  animatedRef: any;
-  private _panResponder: PanResponderInstance;
+    animatedRef: any;
+    private _panResponder: PanResponderInstance;
+    private currentPosition: number;
 
-  constructor(props, state) {
-    super(props, state);
+    constructor(props, state) {
+        super(props, state);
 
-    const position = new Animated.ValueXY();
-    this._panResponder = PanResponder.create({
-      onMoveShouldSetPanResponderCapture: (e, g) => this.onMoveShouldSetPanResponderCapture(e, g),
-      onMoveShouldSetPanResponder: (e, g) => this.onMoveShouldSetPanResponder(e, g),
-      onPanResponderMove: (ev, gesture) => this.onPanResponderMove(ev, gesture),
-      onPanResponderEnd: () => this.onPanResponderEnd()
-    });
+        this._panResponder = PanResponder.create({
+            onMoveShouldSetPanResponderCapture: (e, g) => this.onMoveShouldSetPanResponderCapture(e, g),
+            onMoveShouldSetPanResponder: (e, g) => this.onMoveShouldSetPanResponder(e, g),
+            onPanResponderMove: (ev, gesture) => this.onPanResponderMove(ev, gesture),
+            onPanResponderEnd: () => this.onPanResponderEnd()
+        });
 
-    this.state = {
-      position: position,
-      ...this.swipeSettingsFromProps(this.props)
-    };
-  }
-
-  componentWillReceiveProps(nextProps: Readonly<SwipeToRemoveProps>, nextContext: any): void {
-    this.setState(this.swipeSettingsFromProps(nextProps));
-  }
-
-  render() {
-    return <View style={SwipeActionsStyles.MAIN_CONTAINER}>
-      <View style={[SwipeActionsStyles.DRAGGABLE_BACKGROUND, {backgroundColor: this.state.onSwipeBackgroundColor}]}>
-        <View style={SwipeActionsStyles.DRAGGABLE_BACKGROUND_CONTENT}>
-          <MaterialIcons color={this.props.actionIconSwipeRightColor || this.props.actionIconColor}
-                         size={32}
-                         name={this.props.actionIconSwipeRight || this.props.actionIcon}/>
-          <MaterialIcons color={this.props.actionIconSwipeLeftColor || this.props.actionIconColor}
-                         size={32}
-                         name={this.props.actionIconSwipeLeft || this.props.actionIcon}/>
-        </View>
-      </View>
-      <Animated.View ref={view => this.animatedRef = view} style={[{
-        backgroundColor: this.props.elementBackgroundColor,
-        minWidth: this.props.elementWidth,
-        maxWidth: this.props.elementWidth,
-      }, this.state.position.getLayout()]} {...this._panResponder.panHandlers}>
-        {this.props.children}
-      </Animated.View>
-    </View>;
-  }
-
-  private onPanResponderMove(ev: GestureResponderEvent, gesture: PanResponderGestureState) {
-    Animated.timing(this.state.position, {
-      toValue: {x: gesture.dx, y: 0},
-      duration: 15
-    }).start();
-  }
-
-  private onPanResponderEnd() {
-    const currentPosition = this.state.position.x['_value'];
-    const wasFullSwipe = Math.abs(currentPosition) > this.props.elementWidth / 3;
-    this.setState({
-      onSwipeBackgroundColor: this.props.elementBackgroundColor || ''
-    });
-    if (!wasFullSwipe) {
-      Animated.timing(this.state.position, {
-        toValue: {x: 0, y: 0},
-        duration: 250
-      }).start();
-      return;
+        this.state = {
+            position: new Animated.ValueXY(),
+            animated: new Animated.Value(0),
+            ...this.swipeSettingsFromProps(this.props)
+        };
     }
 
-    Animated.parallel([
-      Animated.timing(this.state.position, {
-        toValue: {x: currentPosition > 0 ? this.props.elementWidth : -this.props.elementWidth, y: 0},
-        duration: 250
-      }),
-    ]).start(result => {
-      if (this.props.onSwipeEnd) {
-        this.props.onSwipeEnd();
-      }
+    componentWillReceiveProps(nextProps: Readonly<SwipeToRemoveProps>, nextContext: any): void {
+        this.setState(this.swipeSettingsFromProps(nextProps));
+    }
 
-      if (currentPosition > 0 && this.props.onSwipeRightEnd) {
-        this.props.onSwipeRightEnd();
-      }
+    render() {
+        let animatedBackground = {
+            backgroundColor: this.state.animated.interpolate({
+                inputRange: [-1, 0, 1],
+                outputRange: [this.props.elementSwipingBackgroundColor, this.props.elementBackgroundColor,
+                    this.props.elementSwipingBackgroundColor],
+                extrapolate: 'clamp'
+            })
+        };
+        let animatedViewStyle = [
+            {
+                backgroundColor: this.props.elementBackgroundColor,
+                minWidth: this.props.elementWidth,
+                maxWidth: this.props.elementWidth
+            },
+            {
+                transform: [
+                    {
+                        translateX: this.state.animated.interpolate({
+                            inputRange: [-1, 1],
+                            outputRange: [-this.props.elementWidth, this.props.elementWidth],
+                            extrapolate: 'clamp'
+                        })
+                    }
+                ]
+            }
+        ];
 
-      if (currentPosition > 0 && this.props.onSwipeLeftEnd) {
-        this.props.onSwipeLeftEnd();
-      }
-    });
-  }
 
-  private onMoveShouldSetPanResponder(ev: GestureResponderEvent, gesture: PanResponderGestureState) {
-    this.setState({
-      onSwipeBackgroundColor: this.props.elementSwipingBackgroundColor || ''
-    });
-    const isThresholdExceeded = Math.abs(gesture.dx) > 10;
-    return (isThresholdExceeded && this.state.enableSwipe) ||
-      (isThresholdExceeded && this.state.enableSwipeLeft && gesture.dx < 0) ||
-      (isThresholdExceeded && this.state.enableSwipeRight && gesture.dx > 0);
-  }
+        return <View style={SwipeActionsStyles.MAIN_CONTAINER}>
+            <Animated.View style={[SwipeActionsStyles.DRAGGABLE_BACKGROUND, {backgroundColor: this.state.onSwipeBackgroundColor}]}>
+                <View style={SwipeActionsStyles.DRAGGABLE_BACKGROUND_CONTENT}>
+                    <MaterialIcons color={this.props.actionIconSwipeRightColor || this.props.actionIconColor}
+                                   size={32}
+                                   name={this.props.actionIconSwipeRight || this.props.actionIcon}/>
+                    <MaterialIcons color={this.props.actionIconSwipeLeftColor || this.props.actionIconColor}
+                                   size={32}
+                                   name={this.props.actionIconSwipeLeft || this.props.actionIcon}/>
+                </View>
+            </Animated.View>
+            <Animated.View ref={view => this.animatedRef = view} style={animatedViewStyle} {...this._panResponder.panHandlers}>
+                {this.props.children}
+            </Animated.View>
+        </View>;
+    }
 
-  private onMoveShouldSetPanResponderCapture(ev: GestureResponderEvent, gesture: PanResponderGestureState) {
-    const isThresholdExceeded = Math.abs(gesture.dx) > 10;
-    return (isThresholdExceeded && this.state.enableSwipe) ||
-      (isThresholdExceeded && this.state.enableSwipeLeft && gesture.dx < 0) ||
-      (isThresholdExceeded && this.state.enableSwipeRight && gesture.dx > 0);
-  }
+    private onPanResponderMove(ev: GestureResponderEvent, gesture: PanResponderGestureState) {
+        this.currentPosition = gesture.dx / (this.props.elementWidth);
+        Animated.timing(this.state.animated, {
+            toValue: gesture.dx / (this.props.elementWidth),
+            duration: 5
+        }).start();
+    }
 
-  private swipeSettingsFromProps(nextProps: Readonly<SwipeToRemoveProps>) {
-    const enableSwipeValue = !!nextProps.enableSwipe;
-    const enableSwipeLeftValue = !!nextProps.enableSwipeLeft;
-    const enableSwipeRightValue = !!nextProps.enableSwipeRight;
-    const isSwipeEnabled = nextProps.hasOwnProperty('enableSwipe') ? enableSwipeValue : true;
+    private onPanResponderEnd() {
+        const wasFullSwipe = Math.abs(this.currentPosition) > 1 / 2.5;
+        if (!wasFullSwipe) {
+            Animated.timing(this.state.animated, {
+                toValue: 0,
+                duration: this.state.animationDuration
+            }).start(() => {
+                this.setState({
+                    onSwipeBackgroundColor: this.props.elementBackgroundColor || ''
+                });
+            });
+            this.currentPosition = 0;
+            return;
+        }
 
-    return {
-      onSwipeBackgroundColor: nextProps.elementBackgroundColor || '',
-      enableSwipe: isSwipeEnabled,
-      enableSwipeLeft: nextProps.hasOwnProperty('enableSwipeLeft') ? enableSwipeLeftValue : isSwipeEnabled,
-      enableSwipeRight: nextProps.hasOwnProperty('enableSwipeRight') ? enableSwipeRightValue : isSwipeEnabled
-    };
-  }
+        Animated.timing(this.state.animated, {
+            toValue: this.currentPosition > 0 ? 1 : -1,
+            duration: this.state.animationDuration
+        }).start(result => {
+            this.currentPosition = this.currentPosition / this.currentPosition;
+            if (this.props.onSwipeEnd) {
+                this.props.onSwipeEnd();
+            }
+
+            if (this.currentPosition > 0 && this.props.onSwipeRightEnd) {
+                this.props.onSwipeRightEnd();
+            }
+
+            if (this.currentPosition > 0 && this.props.onSwipeLeftEnd) {
+                this.props.onSwipeLeftEnd();
+            }
+        });
+    }
+
+    private onMoveShouldSetPanResponder(ev: GestureResponderEvent, gesture: PanResponderGestureState) {
+        const isThresholdExceeded = Math.abs(gesture.dx) > 10;
+        return (isThresholdExceeded && this.state.enableSwipe) ||
+            (isThresholdExceeded && this.state.enableSwipeLeft && gesture.dx < 0) ||
+            (isThresholdExceeded && this.state.enableSwipeRight && gesture.dx > 0);
+    }
+
+    private onMoveShouldSetPanResponderCapture(ev: GestureResponderEvent, gesture: PanResponderGestureState) {
+        if (this.state.onSwipeBackgroundColor !== this.props.elementSwipingBackgroundColor) {
+            this.setState({
+                onSwipeBackgroundColor: this.props.elementSwipingBackgroundColor || ''
+            });
+        }
+
+        const isThresholdExceeded = Math.abs(gesture.dx) > 10;
+        return (isThresholdExceeded && this.state.enableSwipe) ||
+            (isThresholdExceeded && this.state.enableSwipeLeft && gesture.dx < 0) ||
+            (isThresholdExceeded && this.state.enableSwipeRight && gesture.dx > 0);
+    }
+
+    private swipeSettingsFromProps(nextProps: Readonly<SwipeToRemoveProps>) {
+        const enableSwipeValue = !!nextProps.enableSwipe;
+        const enableSwipeLeftValue = !!nextProps.enableSwipeLeft;
+        const enableSwipeRightValue = !!nextProps.enableSwipeRight;
+        const isSwipeEnabled = nextProps.hasOwnProperty('enableSwipe') ? enableSwipeValue : true;
+
+        return {
+            onSwipeBackgroundColor: nextProps.elementBackgroundColor || '',
+            enableSwipe: isSwipeEnabled,
+            animationDuration: nextProps.animationDuration || 250,
+            enableSwipeLeft: nextProps.hasOwnProperty('enableSwipeLeft') ? enableSwipeLeftValue : isSwipeEnabled,
+            enableSwipeRight: nextProps.hasOwnProperty('enableSwipeRight') ? enableSwipeRightValue : isSwipeEnabled
+        };
+    }
 }
